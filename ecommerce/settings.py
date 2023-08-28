c35742ffabeef3909eae53e2230ce7e22cca8136
selvynessato <137649394+selvynessato@users.noreply.github.com>
2023-08-28 09:01:20 -0600

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kb*flvn$vlrwwa!!+5t!01ojq%k0k33t1uq+loerq(1_-&e*8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['82.180.139.30']
+ALLOWED_HOSTS = ['62.72.26.241']
 #ALLOWED_HOSTS = ['*']
 
 
