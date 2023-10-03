10b4440d6fd85de997da9cc72bb33f6c9c103276
ezelvin <138849014+ezelvin@users.noreply.github.com>
2023-10-02 22:55:00 -0600

    02-10-2023

--- static/js/sweetAlert.js
+++
@@ -1,9 +0,0 @@
-function mostrarMensaje() {
-    Swal.fire({
-        position: 'top-center',
-        icon: 'success',
-        title: 'Tu compra se ha añadido al carrito',
-        showConfirmButton: false,
-        timer: 2000
-    });
-}
